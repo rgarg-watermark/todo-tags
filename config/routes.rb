38731139e831457ea8f85fb1479bed34bac8aa7d
rgarg@-atermark <rgarg@watermarkinsights.com>
2020-09-21 18:38:45 +0530

    removed extra route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
       put :restore
     end
   end
-  resources :todo_items, :except => [:new, :edit, :update, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
